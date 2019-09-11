340ffadbde741f0af2f98f1eb7ac5976052d4adf
unknown <eliscdev@gmail.com>
2019-09-11 13:50:32 +0800

    original

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      isShowPath:false,
-      bntLabel:'显示轨迹'
+      charSelected: false,
+      showTraj:0,
+      bntLabel:'显示轨迹',
+      speedChange:false,
+      speed:101
     }
   }
   handleShowPath(){
-    this.setState({isShowPath:!this.state.isShowPath,bntLabel:this.state.isShowPath?'显示轨迹':'暂停播放'})
+    this.setState({showTraj:this.state.showTraj+1,bntLabel:this.state.charSelected?this.state.showTraj%2===0?'暂停':'继续':'显示轨迹',speedChange:false})
+  }
+  handleCharSelect(){
+    this.setState({charSelected:!this.state.charSelected,showTraj:0, bntLabel:'显示轨迹',speedChange:false})
+  }
+  handleReset(){
+    this.setState({showTraj:0,bntLabel:'重新显示',speedChange:false})
+  }
+  handleSpeedDown(){
+    if(this.state.speed < 191) this.setState({speed:this.state.speed+10,speedChange:true})
+  }
+  handleSpeedUp(){
+    if(this.state.speed > 1) this.setState({speed:this.state.speed-10,speedChange:true})
   }
   render(){
     return (
       <div className="App">
+        <button onClick={this.handleCharSelect.bind(this)}>{"白居易"}</button>
         <button onClick={this.handleShowPath.bind(this)}>{this.state.bntLabel}</button>
-        <MainBox showPath={this.state.isShowPath}/>
+        <button onClick={this.handleSpeedDown.bind(this)}>{'减速'}</button>
+        <label>{'×'+((101-this.state.speed)/100+1).toFixed(1)}</label>
+        <button onClick={this.handleSpeedUp.bind(this)}>{'加速'}</button>
+        
+        <MainBox showChar={this.state.charSelected} showTraj={this.state.showTraj} speed={this.state.speed} speedChange={this.state.speedChange} reset={this.handleReset.bind(this)}/>
+        
       </div>
     );
   }
--- src/mainBox.js
+++ src/mainBox.js
@@ -7,71 +7,99 @@ class MainBox extends Component{
     this.dojoUrl = "http://tony-space.top:8007/arcgis_js_api/library/4.11/dojo/dojo.js"
     this.tileMapUrl = "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer"
     this.baseFeatureUrl = "http://172.20.32.70:6080/arcgis/rest/services/TD/MapServer/"
+    this.charHighlight = []
   }
   componentWillMount(){
     this.initMap()
   }
+  
   componentDidUpdate(){
-    if(this.props.showPath){
-      this.showPath()
+    this.charLayer.visible = this.props.showChar
+    if(this.props.showChar){
+      if(this.props.showTraj===0){
+        console.log('loading...')
+        this.polyline.paths = []
+        this.loadingPath()
+      }else if(this.props.showTraj%2!==0){
+        console.log('showing trajectory...')
+        if(this.props.speedChange){
+          if(this.t) {
+            clearInterval(this.t)
+            this.t = null
+          }
+        }
+        if(this.traj && this.traj.length>0) {
+          this.play()
+        }
+      }else{
+        console.log('pausing...')
+        if(this.t) {
+          clearInterval(this.t)
+          this.t = null
+        }
+      }
+    }else{
+      console.log('teminating...')
+      if(this.t) {
+        clearInterval(this.t)
+        this.t = null
+      }
+      this.graphic.geometry = undefined
+      this.polyline.paths = []
+      while(this.charHighlight.length) this.charHighlight.shift().remove()
     }
   }
-  showPath(){
-    this.view.whenLayerView(this.charLayer).then((layerView)=>{
+  play(){
+    //this.view.goTo({center:this.traj[0],zoom:6},{duration:500,easing:'in-out-expo'})
+    this.t = setInterval(() => {
+        //console.log(this.charHighlight)
+        if(this.traj.length>0) {
+          let tmp =  this.traj.shift()
+          //this.view.goTo({center:tmp,zoom:6},{duration:1000,easing:'in-out-expo'})
+          this.polyline.paths.push(tmp)
+          this.graphic.geometry = this.polyline
+          let highlightIdx = this.nodeIdx.shift()
+          if(this.polyline.paths.length===highlightIdx){
+            if(this.charHighlight.length)this.charHighlight.shift().remove()
+            this.view.whenLayerView(this.charLayer).then((layerView)=>{
+            let query = this.charLayer.createQuery();
+            this.charLayer.queryFeatures(query).then((result)=>{
+            this.charHighlight.push(layerView.highlight(result.features.length-this.nodeIdx.length-1));
+            if(!this.traj.length){
+              setTimeout(()=>{
+                while(this.charHighlight.length) this.charHighlight.shift().remove()
+              },800)
+            }
+            //
+            })
+          });
+        }else{
+          this.nodeIdx.unshift(highlightIdx)
+        } 
+        }else{
+          clearInterval(this.t)
+          if(this.props.reset) this.props.reset()
+        }
+    }, this.props.speed);
+  }
+  loadingPath(){
+    this.view.whenLayerView(this.charLayer).then(()=>{
       let queryChar = this.charLayer.createQuery();
       this.charLayer.queryFeatures(queryChar).then((result)=>{
         let feature = result.features
-        console.log(feature)
-        this.view.goTo({
-          center:[feature[0].geometry.x,feature[0].geometry.y],
-          zoom: 6
-        },{
-          duration:1000,
-          easing:'in-out-expo'
-        })
-        setTimeout(()=>{
-          this.drawLine(feature,layerView)
-        },1500)
+        this.traj = []
+        this.cnt = 0
+        this.nodeIdx = [1]
+        for(let i=0;i<feature.length-1;i++){
+          this.traj.push.apply(this.traj,this.interpolation(feature[i].geometry,feature[i+1].geometry,i%2===0?1:-1))
+          this.nodeIdx.push(this.cnt)
+        }
       })
     })
   }
-  drawLine(feature, layerView){
-    console.log('drawing...')
-    let traj = []
-    for(let i=0;i<feature.length-1;i++){
-      traj.push.apply(traj,this.interpolation(feature[i].geometry,feature[i+1].geometry,i%2===0?1:-1))
-    }
-    var lineSymbol = {
-      type: "simple-line", // autocasts as SimpleLineSymbol()
-      color: [226, 119, 40],
-      width: 1.5
-    };
-    var polyline = {
-      type:'polyline',
-      paths:[]
-    }
-    this.view.graphics.add(this.graphic)
-    this.graphic.symbol = lineSymbol
-    let draw = ()=>{
-      if(this.props.showPath && traj.length>0){
-        this.t = setTimeout(()=>{
-          let tmp = traj.shift()
-          polyline.paths.push(tmp)
-          this.view.goTo({center:[tmp[0]+3,tmp[1]],zoom:6},{duration:200,easing: 'in-out-expo'})
-          this.graphic.geometry = polyline
-          draw()
-        },100)
-      }else {
-        clearTimeout(this.t)
-      }
-    }
-    draw()
-  }
-
   interpolation(pointA, pointB,inverse){
     //calculate center
     let angle =(Math.PI/6 + Math.round(Math.random())/5)
-    console.log(angle)
     let pointC  = {x:0,y:0}
     if(pointA.x===pointB.x){
       pointC.x = pointA.x+Math.abs(pointB.y-pointA.y)/2*Math.tan(angle)
@@ -96,9 +124,11 @@ class MainBox extends Component{
       pnt.x = (1-t)*tmpPnt1.x+t*tmpPnt2.x
       pnt.y = (1-t)*tmpPnt1.y+t*tmpPnt2.y
       pntSet.push([pnt.x,pnt.y])
+      this.cnt++
       t+=incre
     }
     pntSet.push([pointB.x,pointB.y])
+    this.cnt++
     return pntSet
   }
   
@@ -121,7 +151,8 @@ class MainBox extends Component{
       });
       this.charLayer = new FeatureLayer({
         url:"http://172.20.32.70:6080/arcgis/rest/services/TD/MapServer/14",
-        definitionExpression:'ID=0'
+        definitionExpression:'ID=0',
+        visible: false
       });
       this.graphic = new Graphic();
       this.map = new Map({
@@ -135,6 +166,17 @@ class MainBox extends Component{
         container: "mapDiv",
         zoom: 5
       });
+      this.graphic.symbol = {
+        type: "simple-line",
+        color: [226, 119, 40],
+        width: 1.5
+      };
+      this.view.graphics.add(this.graphic)
+      this.polyline = {
+        type:'polyline',
+        paths:[]
+      }
+      
     })
   }
 
